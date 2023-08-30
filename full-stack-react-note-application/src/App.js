da7af2430e2a5d3a876e346b9b2255ce3314937b
athrala <argilethrala12@gmail.com>
2023-08-30 01:28:10 -0700

    changes for v2

--- full-stack-react-note-application/src/App.js
+++ full-stack-react-note-application/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,17 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello from V2</h1>
       </header>
     </div>
   );
