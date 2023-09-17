9d0559addcf9e891e671f2c0fe5be0f3a817a7da
athrala <110925591+athrala@users.noreply.github.com>
2023-09-17 14:38:53 -0700

    sidebar isPro={isPro}

--- ai-saas/app/(dashboard)/layout.tsx
+++ ai-saas/app/(dashboard)/layout.tsx
@@ -10,8 +10,8 @@ const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full relative">
       <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-80 bg-gray-900">
-        <Sidebar apiLimitCount={apiLimitCount} />
-        isPro={isPro}
+        <Sidebar isPro={isPro} apiLimitCount={apiLimitCount} />
+        
       </div>
       <main className="md:pl-72 pb-10">
         <Navbar />
